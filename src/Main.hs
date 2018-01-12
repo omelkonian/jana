9d870a4a9f3e8e35ed9aa3b1c6027c43416a53d4
Orestis Melkonian <melkon.or@gmail.com>
2018-01-12 19:01:40 +0100

    Benchmarking

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DoAndIfThenElse #-}
+import Criterion.Main
+import System.IO.Silently (silence)
 
 import System.Environment
 import System.Exit
@@ -14,18 +17,26 @@ import Jana.Invert
 data Options = Options
   { timeOut :: Int
   , invert :: Bool
-  , evalOpts :: EvalOptions }
+  , evalOpts :: EvalOptions
+  , benchmark :: Bool
+  , output :: String
+  }
 
 defaults = Options
   { timeOut = -1
   , invert = False
-  , evalOpts = defaultOptions }
+  , evalOpts = defaultOptions
+  , benchmark = False
+  , output = "report.html"
+  }
 
 usage = "usage: jana [options] <file>\n\
         \options:\n\
         \  -m           use 32-bit modular arithmetic\n\
         \  -tN          timeout after N seconds\n\
-        \  -i           print inverted program"
+        \  -i           print inverted program\n\
+        \  -b           run benchmarking\n\
+        \  -o           output file"
 
 parseArgs :: IO (Maybe ([String], Options))
 parseArgs =
@@ -48,7 +59,9 @@ addOption opts ('-':'t':time) =
   case reads time of
     [(timeVal, "")] -> return $ opts { timeOut = timeVal }
     _               -> Left "non-number given to -t option"
+addOption opts ('-':'o':output) = return opts { output = output }
 addOption opts "-i" = return opts { invert = True }
+addOption opts "-b" = return opts { benchmark = True }
 addOption _ f = Left $ "invalid option: " ++ f
 
 loadFile :: String -> IO String
@@ -72,11 +85,21 @@ parseAndRun filename evalOptions =
 main :: IO ()
 main = do args <- parseArgs
           case args of
-            Just ([file], Options { invert = True }) -> printInverted file
-            Just ([file], opts) ->
-              do res <- timeout (timeOut opts * 1000000)
-                                (parseAndRun file (evalOpts opts))
-                 case res of
-                   Nothing -> exitWith $ ExitFailure 124
-                   _       -> return ()
+            Just ([file], opts) -> do
+              when (invert opts) $ printInverted file
+              text <- loadFile file
+              case parseProgram file text of
+                Left err   -> print err >> (exitWith $ ExitFailure 1)
+                Right prog ->
+                  if benchmark opts then
+                    withArgs ["-o", output opts] $
+                      defaultMain [bgroup file [bench "1" $ whnfIO (go file prog opts)]]
+                  else
+                  go file prog opts
             _ -> putStrLn usage
+          where go file prog opts = do
+                  res <- timeout (timeOut opts * 1000000) (runProgram file prog (evalOpts opts))
+                  print res
+                  case res of
+                    Nothing -> exitWith $ ExitFailure 124
+                    Just r  -> return ()
